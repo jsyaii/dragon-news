02066ea5cf6ca48ba6c217680df9d32ec9d2e821
jsyaii <jasiahasan1703@gmail.com>
2025-10-20 23:51:16 +0600

    login and registers

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,46 @@
-import React from 'react';
-import { NavLink } from 'react-router';
+import React, { use } from "react";
+import { Link, NavLink } from "react-router";
+import { AuthContext } from "../../provider/AuthProvider";
+
 
 const Navbar = () => {
-    return (
-        <div>
-            <div className="navbar bg-base-100 shadow-sm items-center">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+  const { user, logOut } = use(AuthContext);
+  const handleLogOut = () => {
+    console.log("user trying to LogOut");
+    logOut()
+      .then(() => {
+        alert("You Logged Out successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div className="flex justify-between items-center">
+      <div className="">{user && user.email}</div>
+      <div className="nav flex gap-5 text-accent">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/career">Career</NavLink>
       </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content text-accent bg-base-100 rounded-box z-1 mt-3 w-52 p-2 gap-5 shadow">
-       <NavLink to="/">Home</NavLink>
-       <NavLink to="/about">About</NavLink>
-       <NavLink to="/career">Career</NavLink>
-      </ul>
+      <div className="login-btn flex gap-5">
+        <img
+          className="w-12 rounded-full"
+          src="../src/assets/user.png"
+          alt=""
+        />
+        {user ? (
+          <button onClick={handleLogOut} className="btn btn-primary px-10 ">
+            LogOut
+          </button>
+        ) : (
+          <Link to="/auth/login" className="btn btn-primary px-10 ">
+            Login
+          </Link>
+        )}
+      </div>
     </div>
-   
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal text-accent px-1 gap-5">
-     <NavLink to="/">Home</NavLink>
-       <NavLink to="/about">About</NavLink>
-       <NavLink to="/career">Career</NavLink>
-       </ul>
-  </div>
-  <div className="navbar-end gap-2">
-    <div>
-        <img src="../src/assets/user.png" alt="" />
-    </div>
-    <a className="btn">Button</a>
-  </div>
-</div>
-        </div>
-    );
-}
+  );
+};
 
 export default Navbar;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8kyEe769zSNaW6nfwAvQWqFws83i16C4",
+  authDomain: "dragon-news-d2846.firebaseapp.com",
+  projectId: "dragon-news-d2846",
+  storageBucket: "dragon-news-d2846.firebasestorage.app",
+  messagingSenderId: "204459185857",
+  appId: "1:204459185857:web:5dd219c96f34ec57342640"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
